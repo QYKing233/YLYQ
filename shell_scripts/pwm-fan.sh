1821cc3cef7f4e0b0ac4e1843977251d9579cffa
QYKing233 <61415390+QYKing233@users.noreply.github.com>
2024-08-18 16:08:31 +0800

    更新 pwm-fan.sh

--- shell_scripts/pwm-fan.sh
+++ shell_scripts/pwm-fan.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-# 检查 GPIO226 目录是否存在
-if [! -d /sys/class/gpio/gpio226]; then
-    echo 226 > /sys/class/gpio/export
+# 检查 GPIO74 目录是否存在
+if [! -d /sys/class/gpio/gpio74]; then
+    echo 74 > /sys/class/gpio/export
     sleep 2
-    echo out > /sys/class/gpio/gpio226/direction
+    echo out > /sys/class/gpio/gpio74/direction
 fi
 
 # 定义温度阈值和相应操作
@@ -16,11 +16,11 @@ while true; do
     temp=$(cat /sys/class/thermal/thermal_zone0/temp)
     if [ $temp -gt $high_threshold_temp ]; then
         # CPU 温度大于 55°C，设置最大速度
-        echo -n 1 > /sys/class/gpio/gpio226/value
+        echo -n 1 > /sys/class/gpio/gpio74/value
         sleep 5
     elif [ $temp -lt $low_threshold_temp ]; then
         # CPU 温度低于等于 45°C，关闭风扇
-        echo -n 0 > /sys/class/gpio/gpio226/value
+        echo -n 0 > /sys/class/gpio/gpio74/value
     fi
     
     sleep 2
