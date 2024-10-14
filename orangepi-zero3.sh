74844ed976e1a70acc7c318920421b8f9c97c3fa
QYKing233 <qyking233@gmail.com>
2024-10-14 11:12:22 +0800

    更新脚本

--- orangepi-zero3.sh
+++ orangepi-zero3.sh
@@ -143,7 +143,7 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 
 
 # 为 luci-app-Alist 调整 golang 版本
-sudo apt-get install libfuse-dev
+sudo apt-get install libfuse-dev && y
 rm -rf ./feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
