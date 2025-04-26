bb091ce413ac896ccb3cdf6648ccd121f14653b2
QYKing233 <41163804@qq.com>
2025-04-26 11:59:23 +0800

    add orangepi-zero2

--- orangepi-zero2.sh
+++ orangepi-zero2.sh
@@ -292,12 +292,12 @@ cp ./patch/A002-orangepi-fix-yt8531C-phy.patch ./target/linux/sunxi/patches-6.1/
 cp ./patch/A003-orangepi-fix-yt8531C-phy.patch ./target/linux/sunxi/patches-6.1/
 
 
-# 添加 orangepi-zero3 config
+# 添加 orangepi-zero2 config
 rm -rf ./.config
-mv ./YLYQ/orangepi-zero3.config ./
+mv ./YLYQ/orangepi-zero2.config ./
 # mv ./YLYQ/dae.config ./
 # cat ./dae.config >> ./orangepi-zero3.config
-mv ./orangepi-zero3.config ./.config
+mv ./orangepi-zero2.config ./.config
 
 
 # 删除 YLYQ
